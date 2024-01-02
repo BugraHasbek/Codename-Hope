c7a857103373d83ef39b919e14f52df63b022b42
Bugra Hasbek <bugrahasbek@gmail.com>
2024-01-03 00:27:19 +0100

    typo

--- scene_manager.hpp
+++ scene_manager.hpp
@@ -22,7 +22,7 @@ namespace rendering
 		sf::Texture empty_texture;
 		sf::Texture green_texture;
 		sf::Texture invalid_texture;
-		std::array<unsigned int, world_size_x* world_size_y> isometric_world;
+		std::array<unsigned int, world_size_x * world_size_y> isometric_world;
 
 		std::pair<float, float> world2Screen(const unsigned int& x, const unsigned int& y) const;
 		sf::Vector2u screen2World(const sf::Vector2i& mouse_pos, sf::Vector2f top_left_corner) const;
