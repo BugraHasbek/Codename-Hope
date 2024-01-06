f727acc5cf09ed0bdd4dfb4f3fc5655e885c9488
Bugra Hasbek <bugrahasbek@gmail.com>
2024-01-06 01:08:49 +0100

    move character around

--- character.cpp
+++ character.cpp
@@ -15,10 +15,30 @@ GameObjects::character::character()
 
 void GameObjects::character::draw(sf::RenderWindow& window)
 {
+    // TODO: Handle keyboard input
+    // I was planning to handle keyboard events in game.cpp but perhaps it is overengineering. I think each ondividual game object can handle events individually
+    // TODO: calculate delta time.
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+    {
+        location.y -= 1;
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+    {
+        location.y += 1;
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+    {
+        location.x -= 1;
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+    {
+        location.x += 1;
+    }
+
 	if(current_status == status::idle)
 	{
 		sf::Sprite sprite = idle_animations.at(animation_index / 150);
-		sprite.setPosition(location);
+		sprite.setPosition(location);   // TODO: we need a transformation here.. chracter class should know position in term of game world coordinates. actual isometric coordinates should be used only before drawing sprite
 		window.draw(sprite);
 		animation_index = (animation_index + 1) % (idle_animations.size() * 150);
 	}
